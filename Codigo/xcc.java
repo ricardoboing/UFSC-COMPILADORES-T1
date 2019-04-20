37f30feacc744806dbf7374292b9e4f851565eca
Ricardo Boing <ricardoboing.ufsc@gmail.com>
2019-04-19 23:28:27 -0300

    Correcao: ao inves de salvar lexemas agora salva o nome do token no arquivo ListaDeToken.txt.

--- Codigo/xcc.java
+++ Codigo/xcc.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
 
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.*;
 
 public class xcc {
 	public static void main(String[] args) throws IOException {
@@ -20,6 +17,8 @@ public class xcc {
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		tokens.getNumberOfOnChannelTokens();
 		
+		Vocabulary voc = lexer.getVocabulary();
+		
 		List<Token> listTokens = tokens.getTokens();
 		FileWriter novoArquivo;
 		
@@ -30,7 +29,7 @@ public class xcc {
 				Token token = listTokens.get(c);
 				
 				if (token.getText() != "<EOF>") {
-					novoArquivo.write(token.getText()+"\n");
+					novoArquivo.write(voc.getSymbolicName(token.getType())+"\n");
 				}
 			}
 			
