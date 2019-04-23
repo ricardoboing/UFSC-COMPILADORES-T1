e1bbd991b0d0fcc2cacdb48637306f43cb95c63b
Ricardo Boing <ricardoboing.ufsc@gmail.com>
2019-04-23 19:55:44 -0300

    Add tabela de simbolos; Distribuicao de responsabilidades em classes; Adaptacoes no makefile; Remocao de erro lexico no arquivo fonte.xpp.

--- Codigo/xcc.java
+++ Codigo/xcc.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
 
-import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.Vocabulary;
 
 public class xcc {
 	public static void main(String[] args) throws IOException {
@@ -17,25 +21,48 @@ public class xcc {
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		tokens.getNumberOfOnChannelTokens();
 		
-		Vocabulary voc = lexer.getVocabulary();
+		Vocabulary vocabularioAntlr = lexer.getVocabulary();
 		
 		List<Token> listTokens = tokens.getTokens();
-		FileWriter novoArquivo;
+		FileWriter arquivoListaDeToken, arquivoTabelaDeSimbolo;
+		
+		TabelaDeSimbolo tabelaDeSimbolo;
+		tabelaDeSimbolo = new TabelaDeSimbolo(vocabularioAntlr);
 		
 		try {
-			novoArquivo = new FileWriter(new File(diretorio+"ListaDeToken.txt"));
+			arquivoListaDeToken = new FileWriter(new File(diretorio+"ListaDeToken.txt"));
+			arquivoTabelaDeSimbolo = new FileWriter(new File(diretorio+"TabelaDeSimbolo.txt"));
 			
+			System.out.println("-------------------- LISTA DE TOKENS --------------------");
 			for (int c = 0; c < listTokens.size(); c++) {
 				Token token = listTokens.get(c);
 				
 				if (token.getText() != "<EOF>") {
-					novoArquivo.write(voc.getSymbolicName(token.getType())+"\n");
+					String nomeDoToken;
+					nomeDoToken = vocabularioAntlr.getSymbolicName(token.getType());
+					
+					arquivoListaDeToken.write(nomeDoToken+"\n");
+					System.out.println(nomeDoToken);
+					
+					tabelaDeSimbolo.addLexema(token);
 				}
 			}
+			System.out.println();
 			
-			novoArquivo.close();
+			String tabelaDeSimboloToString;
+			tabelaDeSimboloToString = tabelaDeSimbolo.getToString();
+			
+			arquivoTabelaDeSimbolo.write(tabelaDeSimboloToString);
+			System.out.println("-------------------- TABELA DE SIMBOLOS --------------------");
+			System.out.println(tabelaDeSimboloToString);
+			
+			arquivoListaDeToken.close();
+			arquivoTabelaDeSimbolo.close();
+			
 		} catch (IOException e) {
-		} catch (Exception e) {
-		}
+		} catch (Exception e) {}
+
+		System.out.println("Update file ListaDeToken.txt");
+		System.out.println("Update file TabelaDeSimbolos.txt");
 	}
 }
